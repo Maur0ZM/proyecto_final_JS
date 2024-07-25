cbddc751f7f0f895f39dc1d5db9ff0e901a01102
Mauro <mauro.zm0@gmail.com>
2024-07-24 23:42:22 -0400

    correciones

--- js/header.js
+++ js/header.js
@@ -4,28 +4,28 @@ const nav = document.createElement("nav");
 const ul = document.createElement("ul");
 
 const contPaginas = {
-        inicio:`<li><a href="">Inicio</a></li>
+        inicio:`<li><a href="#">Inicio</a></li>
                 <li><a href="./pages/productos.html">Productos</a></li>
                 <li><a href="./pages/contacto.html">Contacto</a></li>
                 <li><a href="./pages/carrito.html"><img src="./assent/img/carrito.png" alt=""></a></li>
 
     `,
         productos:`<li><a href="../index.html">Inicio</a></li>
-                    <li><a href="">Productos</a></li>
+                    <li><a href="#">Productos</a></li>
                     <li><a href="contacto.html">Contacto</a></li>
                     <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
 
     `,
         contacto: `<li><a href="../index.html">Inicio</a></li>
                     <li><a href="productos.html">Productos</a></li>
-                    <li><a href="">Contacto</a></li>
+                    <li><a href="#">Contacto</a></li>
                     <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
 
     `, 
         carrito: `  <li><a href="../index.html">Inicio</a></li>
                     <li><a href="productos.html">Productos</a></li>
                     <li><a href="contacto.html">Contacto</a></li>
-                    <li><a href=""><img src="../assent/img/carrito.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assent/img/carrito.png" alt=""></a></li>
     `
 }
 
@@ -38,17 +38,17 @@ nav.classList.add("nav");
 ul.classList.add("ul");
 
 switch (window.location.pathname) {
-    case '/index.html':
+    case './index.html':
     case '/':  
         ul.innerHTML = contPaginas.inicio;
         break;
-    case '/pages/productos.html':
+    case './pages/productos.html':
         ul.innerHTML = contPaginas.productos;
         break;
-    case '/pages/contacto.html':
+    case './pages/contacto.html':
         ul.innerHTML = contPaginas.contacto;
         break;
-    case '/pages/carrito.html':
+    case './pages/carrito.html':
         ul.innerHTML = contPaginas.carrito;
         break;
     default:
