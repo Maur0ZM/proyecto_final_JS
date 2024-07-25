be78f8ee4bd528e86ea733dc4559f591675051d0
Mauro <mauro.zm0@gmail.com>
2024-07-24 23:58:00 -0400

    a

--- js/header.js
+++ js/header.js
@@ -1,58 +1,54 @@
-
 const header = document.createElement("header");
 const nav = document.createElement("nav");
 const ul = document.createElement("ul");
 
 const contPaginas = {
-        inicio:`<li><a href="#">Inicio</a></li>
-                <li><a href="./pages/productos.html">Productos</a></li>
-                <li><a href="./pages/contacto.html">Contacto</a></li>
-                <li><a href="./pages/carrito.html"><img src="./assent/img/carrito.png" alt=""></a></li>
-
+    inicio: `<li><a href="#">Inicio</a></li>
+             <li><a href="./pages/productos.html">Productos</a></li>
+             <li><a href="./pages/contacto.html">Contacto</a></li>
+             <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
     `,
-        productos:`<li><a href="../index.html">Inicio</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="contacto.html">Contacto</a></li>
-                    <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
-
+    productos: `<li><a href="../index.html">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+                <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
     `,
-        contacto: `<li><a href="../index.html">Inicio</a></li>
-                    <li><a href="productos.html">Productos</a></li>
-                    <li><a href="#">Contacto</a></li>
-                    <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
-
-    `, 
-        carrito: `  <li><a href="../index.html">Inicio</a></li>
-                    <li><a href="productos.html">Productos</a></li>
-                    <li><a href="contacto.html">Contacto</a></li>
-                    <li><a href="#"><img src="../assent/img/carrito.png" alt=""></a></li>
+    contacto: `<li><a href="../index.html">Inicio</a></li>
+               <li><a href="productos.html">Productos</a></li>
+               <li><a href="#">Contacto</a></li>
+               <li><a href="carrito.html"><img src="../assent/img/carrito.png" alt=""></a></li>
+    `,
+    carrito: `<li><a href="../index.html">Inicio</a></li>
+              <li><a href="productos.html">Productos</a></li>
+              <li><a href="contacto.html">Contacto</a></li>
+              <li><a href=""><img src="../assent/img/carrito.png" alt=""></a></li>
     `
-}
+};
 
 header.appendChild(nav);
-body.appendChild(header);
+document.body.appendChild(header);
 
-//Agregando clases
+// Agregar clases
 header.classList.add("header");
 nav.classList.add("nav");
 ul.classList.add("ul");
 
 switch (window.location.pathname) {
-    case './index.html':
-    case '/':  
+    case '/index.html':
+    case '/':
         ul.innerHTML = contPaginas.inicio;
         break;
-    case './pages/productos.html':
+    case '/pages/productos.html':
         ul.innerHTML = contPaginas.productos;
         break;
-    case './pages/contacto.html':
+    case '/pages/contacto.html':
         ul.innerHTML = contPaginas.contacto;
         break;
-    case './pages/carrito.html':
+    case '/pages/carrito.html':
         ul.innerHTML = contPaginas.carrito;
         break;
     default:
-        ul.innerHTML = contPaginas.inicio;  // Valor por defecto si no se encuentra la ruta
+        ul.innerHTML = contPaginas.inicio; // Valor por defecto si no se encuentra la ruta
 }
 
 // Añadir ul al nav
